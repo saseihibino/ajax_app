25cc341de0ba9963be52f748e11a84df4f101663
saseihibino <132185275+saseihibino@users.noreply.github.com>
2023-05-12 17:29:06 +0900

    リクエストを処理する

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,8 +1,8 @@
 function post (){
-  const submit = document.getElementById('submit');
-  submit.addEventListener('click', (e) => {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const form = document.getElementById('form');
+    const form = document.getElementById("form");
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
